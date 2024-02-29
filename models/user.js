8f43b124b79b53a5fe8d532eef839436dfcfaaed
firas.houaidi@tek-up.de firas houaidi <houaidifiras.tekup@gmail.com>
2024-02-29 14:15:35 +0100

    auth

--- models/user.js
+++ models/user.js
@@ -3,6 +3,18 @@ const bcrypt = require('bcryptjs')
 const userschema = new mongoose.Schema({
  username:{type:String,uniq:true},
  password:string
-
-
 })
+ userschema.pre('save', async function(next)
+ {
+    const user = this;
+    if(user.isModified('password'))
+    {
+        user.password = await bcrypt.hash(user.password)
+    }
+    next();
+ })
+
+ const user = mongoose.model('user',userschema)
+ module.exports = user ;
+ module.exports = user;
+   
